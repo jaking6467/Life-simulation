7368e9e34d982c6da66122a53cacb274175c44bb
jaking6467 <166968275+jaking6467@users.noreply.github.com>
2026-02-08 19:46:26 +0700

    Update socket.js

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,7 +1,7 @@
 // src/services/socket.js
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || 'http://localhost:3001';
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || 'https://life-simulation-git-main-jelaytamoons-projects.vercel.app/';
 
 class SocketService {
   constructor() {
